e4d17fe748246d12ed9ee7fa3bce4b067d80218f
Keemchard <keemchardtamio@gmail.com>
2022-09-06 11:08:29 +0800

    add nav items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,11 @@ import React from "react";
 const Header = () => {
   return (
     <div className="head">
+      <ul>
+        <li>Best Seller</li>
+        <li>Full Overview</li>
+        <li>Reviews</li>
+      </ul>
       <input type="text" placeholder="Search Book Category here" />
       <button>Search</button>
     </div>
