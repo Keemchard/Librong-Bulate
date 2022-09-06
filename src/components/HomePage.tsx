a424d43ab64c03f8fdc2099ef63ea674ef51bd95
Keemchard <keemchardtamio@gmail.com>
2022-09-06 11:36:50 +0800

    add some words in home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./LB.png";
 
 const HomePage = () => {
+  // const [dayToday, setDayToday] = useState<number>();
+  const date = new Date();
+  let day = date.getDay();
+
+  const week = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const dayToday = week[day];
+
   return (
     <div
       style={{
@@ -15,7 +31,18 @@ const HomePage = () => {
         src={logo}
         alt="logo"
       />
-      <h1 style={{ marginTop: "10px" }}>Home Page</h1>
+      {/* <h1 style={{ marginTop: "10px" }}>Home Page</h1> */}
+      <div style={{ textAlign: "center", marginTop: "10px" }}>
+        <h1 style={{ margin: "10px 0" }}>
+          &#128044;{` Happy ${dayToday}! `}&#128044;
+        </h1>
+        <p>
+          Welcome to{" "}
+          <span style={{ color: "#c3083f", fontWeight: "bold" }}>LB</span>! To
+          explore, simply click anything that is clickable in this Web App
+          &#128516;.
+        </p>
+      </div>
     </div>
   );
 };
